d79b2f6b4d56e4104bc7a08ce036acd986b36069
VinceBaz <VinceBaz@users.noreply.github.com>
2023-11-24 18:23:08 +0000

    Merge pull request #123 from justinehansen/compare-images
    [FIX] stats.compare_images compatible with more metrics e1f4fcb427d6796b9caf1c8904258fef69f7ffe0

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.gabc085a',
+    VERSION: '0+untagged.1.ge1f4fcb',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
